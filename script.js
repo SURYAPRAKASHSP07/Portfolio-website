1d5add5bade90af882bbfa203ae6e7cceb78576d
Surya Prakash S P <151605828+SURYAPRAKASHSP07@users.noreply.github.com>
2024-04-21 03:17:53 -1200

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,32 @@ const copyText = document.querySelectorAll(".copyText");
         
     };
 
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let ProgressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+
+    let calcHeight = 
+     document.documentElement.scrollHeight -
+     document.documentElement.clientHeight;
+
+     let scrollValue = Math.round((pos * 100)/calcHeight);
+
+     if(pos > 100){
+        scrollProgress.style.display = "grid";
+     }
+     else{
+        scrollProgress.style.display = "none";
+     }
+     scrollProgress.addEventListener("click",()=>{
+        document.documentElement.scrollTop = "0";
+     });
+
+     scrollProgress.style.background = `conic-gradient(#ffcc00 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
 
     
-
